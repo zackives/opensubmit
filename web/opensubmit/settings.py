54b7fd1a01f76ed5ba6aa3cf30a40f95316b6d2d
Zack Ives <zives@cis.upenn.edu>
2017-08-08 09:33:13 -0400

    Generic email via gmail

--- web/opensubmit/settings.py
+++ web/opensubmit/settings.py
@@ -340,7 +340,7 @@ GRAPPELLI_INDEX_DASHBOARD = {
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'upenn.cis555'
-EMAIL_HOST_PASSWORD = '16pennapps!'
+EMAIL_HOST_USER = 'my-account'
+EMAIL_HOST_PASSWORD = 'my-pass'
 
 assert(not config.has_section('overrides'))     # factored out
